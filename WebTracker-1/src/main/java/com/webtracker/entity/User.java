c7c8af345567b7d935a08b68dcb13a737ffb2aaf
Anurag26091994 <112397456+Anurag26091994@users.noreply.github.com>
2024-06-28 19:08:55 +0530

    Add NotNull Annotation on username and password

--- WebTracker-1/src/main/java/com/webtracker/entity/User.java
+++ WebTracker-1/src/main/java/com/webtracker/entity/User.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Entity
 public class User {
@@ -17,7 +18,9 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotNull
 	private String username;
+	@NotNull
 	private String password;
 	
 	@OneToMany (mappedBy = "user",cascade = CascadeType.ALL)
